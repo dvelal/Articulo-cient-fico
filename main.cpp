2ab922864ce43b7069e25230f73f684798df43c8
dvelal <dvelal@unjbg.edu.pe>
2025-10-12 15:10:05 -0500

    cambios para evaluar los 2 algoritmos ala vez

--- main.cpp
+++ main.cpp
@@ -7,25 +7,46 @@ using namespace std;
 int main(){
     const int n = 1000;
     int A[n];
+    int B[n];
 
 
     for (long i = 0; i < 30; i ++){
-        if (!leerCSV("array_1000.csv", A, n)) {
+        if (!leerCSV("array_1000.csv", A, n))
             return 1; // error al leer archivo
-        }
+        if (!leerCSV("array_1000.csv", B, n))
+            return 1; // error al leer archivo
 
-        auto inicio = chrono::steady_clock::now();
+        auto inicio1 = chrono::steady_clock::now();
+        InsercionBin(A, n);
+        auto fin1 = chrono::steady_clock::now();
 
-        interDirDer(A, n);
+        
+        auto inicio2 = chrono::steady_clock::now();
+        shellsort(B, n);
+        auto fin2 = chrono::steady_clock::now();
 
-        auto fin = chrono::steady_clock::now();
         cout << "========================" << endl;
-        cout << chrono::time_point_cast<chrono::nanoseconds>(inicio).time_since_epoch().count() << endl; //Punto en el tiempo (inicio del conteo)
-        cout << chrono::time_point_cast<chrono::nanoseconds>(fin).time_since_epoch().count() << endl; //Punto en el tiempo (fin del conteo)
-        cout << "Time difference = " << endl;
-        cout << chrono::duration_cast<chrono::nanoseconds>(fin - inicio).count() << "[ns]" << endl;
-        cout << chrono::duration_cast<chrono::microseconds>(fin - inicio).count() << "[us]" << endl;
-        cout << chrono::duration_cast<chrono::milliseconds>(fin - inicio).count() << "[ms]" << endl;
+        cout << "------------------------" << endl;
+        cout << chrono::time_point_cast<chrono::nanoseconds>(inicio1).time_since_epoch().count() << endl; //Punto en el tiempo (inicio del conteo)
+        cout << chrono::time_point_cast<chrono::nanoseconds>(fin1).time_since_epoch().count() << endl; //Punto en el tiempo (fin del conteo)
+        cout << chrono::time_point_cast<chrono::nanoseconds>(inicio2).time_since_epoch().count() << endl;
+        cout << chrono::time_point_cast<chrono::nanoseconds>(fin2).time_since_epoch().count() << endl;
+        cout << "------------------------" << endl;
+
+        cout << "Tiempo Algoritmo de Insercion Binaria = " << endl;
+        cout << chrono::duration_cast<chrono::nanoseconds>(fin1 - inicio1).count() << "[ns]" << endl;
+        cout << chrono::duration_cast<chrono::microseconds>(fin1 - inicio1).count() << "[us]" << endl;
+        cout << chrono::duration_cast<chrono::milliseconds>(fin1 - inicio1).count() << "[ms]" << endl;
+
+        cout << "Tiempo Algoritmo de RadixSort = " << endl;
+        cout << chrono::duration_cast<chrono::nanoseconds>(fin2 - inicio2).count() << "[ns]" << endl;
+        cout << chrono::duration_cast<chrono::microseconds>(fin2 - inicio2).count() << "[us]" << endl;
+        cout << chrono::duration_cast<chrono::milliseconds>(fin2 - inicio2).count() << "[ms]" << endl;
+    }
+
+    cout << "Arreglo ordenado: ";
+    for (int i = 0; i < n; i++) {
+        cout << B[i] << " ";
     }
     return 0;
 }

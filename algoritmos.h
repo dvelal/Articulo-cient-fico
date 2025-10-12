5aef27bc419af4d6a6e0dec14b8aa3c96c184014
ejyanaca <elvisyanacayo@gmail.com>
2025-10-12 17:00:43 -0500

    Implementamos 2 contadores para los intercambios y comparaciones

--- algoritmos.h
+++ algoritmos.h
@@ -4,20 +4,42 @@
 #include <iostream>
 using namespace std;
 
-void interDirDer(int A[], int n) {
-    int aux;
-    for (int i = 0; i < n - 1; ++i) {     
-        bool swapped = false;
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (A[j] > A[j + 1]) {
-                aux = A[j];
-                A[j] = A[j + 1];
-                A[j + 1] = aux;
-                swapped = true;
+int obtenerMax(int A[], int n, int &comparaciones){
+    int max=A[0];
+    for(int i=1; i<n; i++){
+        comparaciones++; 
+        if(A[i] > max){
+            max=A[i];
+        }
+    }
+    return max;
+}
+
+void RadixSort(int A[], int n, int &comparaciones, int &movimientos){
+    int max = obtenerMax(A, n, comparaciones);
+
+    for(int peso = 1; max/peso > 0; peso = peso * 10){
+        comparaciones++; 
+
+        int urna[10][n];
+        int cuenta[10]={0};
+
+        for(int i = 0; i < n; i++){
+            int digito = (A[i]/peso) % 10;
+            comparaciones++; 
+            urna[digito][cuenta[digito]++] = A[i];
+            movimientos++; 
+        }
+
+        int idx = 0; 
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < cuenta[i]; j++){
+                A[idx++] = urna[i][j];
+                movimientos++; 
             }
         }
-        if (!swapped) break;
     }
 }
+
 
 #endif
